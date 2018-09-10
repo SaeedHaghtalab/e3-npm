40d0ce4b3c9f692b6ed10c26ebbc53521d861b1f
saeed.haghtalab@esss.se <Julen.EtxeberriaMalkorra@esss.se>
2018-09-10 17:56:50 +0200

    wip

--- opi/npm-support/scripts/prefix2Macro.js
+++ opi/npm-support/scripts/prefix2Macro.js
@@ -3,5 +3,5 @@ importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 var macroInput = DataUtil.createMacrosInput(true);
 var prefix = PVUtil.getString(pvArray[0]);
 
-macroInput.put("PREFIX", prefix);
+macroInput.put("secsub", prefix);
 widgetController.setPropertyValue("macros", macroInput);
