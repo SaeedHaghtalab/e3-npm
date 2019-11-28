4040255f6343f253de3907450816e792e9f87644
Saeed Haghtalab <saeed.haghtalab@esss.se>
2019-11-28 14:16:31 +0100

    Move requires from .iocsh to .cmd

--- cmds/lebt-020.cmd
+++ cmds/lebt-020.cmd
@@ -1,5 +1,10 @@
 require npm, 1.0.0
-
+require mrfioc2, 2.2.0-rc7
+require ADProsilica,10bfc26
+require admisc,2.0.0
+require busy,1.7.2-e45eda2
+require sequencer,2.2.7
+require calc,3.7.3
 
 
 epicsEnvSet("SECSUB",   "LEBT-020") 
